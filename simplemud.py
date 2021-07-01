2762464148b2c18a0c7c50ffdb2d0cc63cbeb3d3
Massimiliano De Ruosi <massimiliano.deruosi@uniud.it>
2021-07-01 20:12:12 +0200

    traduzione in italiano di simplemud.py

--- simplemud.py
+++ simplemud.py
@@ -1,226 +1,227 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""A simple Multi-User Dungeon (MUD) game. Players can talk to each
-other, examine their surroundings and move between rooms.
+"""Un semplice gioco Multi-User Dungeon (MUD). I giocatori possono parlare
+fra loro, esaminare l'ambiente e muoversi tra le stanze.
 
-Some ideas for things to try adding:
-    * More rooms to explore
-    * An 'emote' command e.g. 'emote laughs out loud' -> 'Mark laughs
-        out loud'
-    * A 'whisper' command for talking to individual players
-    * A 'shout' command for yelling to players in all rooms
-    * Items to look at in rooms e.g. 'look fireplace' -> 'You see a
-        roaring, glowing fire'
-    * Items to pick up e.g. 'take rock' -> 'You pick up the rock'
-    * Monsters to fight
-    * Loot to collect
-    * Saving players accounts between sessions
-    * A password login
-    * A shop from which to buy items
+Alcune idee sulle cose da aggiungere:
+    * Più stanze da esplorare
+    * Un comando 'emote', ad es. 'emote lol' -> 'Marco scoppia a ridere'
+    * Un comando 'sussurra' per parlare ai singoli giocatori
+    * Un comando 'urla' per gridare ai gicatori in tutte le stanze
+    * Oggetti da osservare nelle stanze, ad esempio
+      'guarda caminetto' -> 'Vedi un fuoco ardente e scoppiettante'
+    * Oggetti da prendere, ad esempio: 'prendi sasso' -> 'Hai preso il sasso'
+    * Mostri da combattere
+    * Tesori da raccogliere
+    * Salvare i dati dei giocatori tra le sessioni
+    * Accesso (login) con password
+    * Un negozio in cui comperare oggetti
 
-author: Mark Frimston - mfrimston@gmail.com
+autore: Mark Frimston - mfrimston@gmail.com
+traduzione italiana: Massimiliano De Ruosi - massimiliano.deruosi@gmail.com
 """
 
 import time
 
-# import the MUD server class
+# importa la classe: MUD server
 from mudserver import MudServer
 
-
-# structure defining the rooms in the game. Try adding more rooms to the game!
-rooms = {
-    "Tavern": {
-        "description": "You're in a cozy tavern warmed by an open fire.",
-        "exits": {"outside": "Outside"},
+# struttura che definisce le stanze nel gioco. Prova ad aggiungere più stanze!
+stanze = {
+    "Taverna": {
+        "descrizione": "Sei in una confortevole taverna riscaldata da un caminetto",
+        "uscite": {"esterno": "Esterno"},
     },
-    "Outside": {
-        "description": "You're standing outside a tavern. It's raining.",
-        "exits": {"inside": "Tavern"},
+    "Esterno": {
+        "descrizione": "Ti trovi fuori da una taverna. Sta piovendo.",
+        "uscite": {"interno": "Taverna"},
     }
 }
 
-# stores the players in the game
-players = {}
+# memorizza i giocatori nel gioco
+giocatori = {}
 
-# start the server
+# avvia il server
 mud = MudServer()
 
-# main game loop. We loop forever (i.e. until the program is terminated)
+# ciclo principale del gioco. Viene eseguito all'infinito (i.e. fino a quando il programma viene terminato)
 while True:
 
-    # pause for 1/5 of a second on each loop, so that we don't constantly
-    # use 100% CPU time
+    # pausa per 1/5 di secondo a ogni ciclo, così non usiamo
+    # costantemente il 100% della CPU
     time.sleep(0.2)
 
-    # 'update' must be called in the loop to keep the game running and give
-    # us up-to-date information
+    # 'update' deve essere chiamato durante il ciclo per mantenere il gioco attivo e con informazioni aggiornate
     mud.update()
 
-    # go through any newly connected players
+    # gestisci i nuovi giocatori che si sono appena connessi
     for id in mud.get_new_players():
 
-        # add the new player to the dictionary, noting that they've not been
-        # named yet.
-        # The dictionary key is the player's id number. We set their room to
-        # None initially until they have entered a name
-        # Try adding more player stats - level, gold, inventory, etc
-        players[id] = {
-            "name": None,
-            "room": None,
+        # aggiungi i nuovi giocatori al dizinario, notare che non hanno
+        # ancora un nome.
+        # La chiave dek dizionario è il numero id del giocatore. Impostiamo
+        # la loro stanza a None inizialmente finché non hanno inserito un nome
+        # Prova ad aggiungere ulteriori statistiche del giocatore - livello,
+        # oro, inventario, ecc.
+        giocatori[id] = {
+            "nome": None,
+            "stanza": None,
         }
 
-        # send the new player a prompt for their name
-        mud.send_message(id, "What is your name?")
+        # invia ad ogni nuovo giocatore la richiesta del prorio nome
+        mud.send_message(id, "Qual è il tuo nome?")
 
-    # go through any recently disconnected players
+    # gestisci i giocatori disconnessi di recente
     for id in mud.get_disconnected_players():
 
-        # if for any reason the player isn't in the player map, skip them and
-        # move on to the next one
-        if id not in players:
+        # se per qualunque ragione il giocatore non è nell'elenco, saltalo e
+        # passa al successivo
+        if id not in giocatori:
             continue
 
-        # go through all the players in the game
-        for pid, pl in players.items():
-            # send each player a message to tell them about the diconnected
-            # player
-            mud.send_message(pid, "{} quit the game".format(
-                                                        players[id]["name"]))
+        # gestisci ogni giocatore nel gioco
+        for gid, gio in giocatori.items():
+            # manda a ogni giocatore un messaggio per informarlo riguardo
+            # i giocatori disconnessi
+            mud.send_message(gid, "{} ha lasciato il gioco".format(
+                                                        giocatori[id]["nome"]))
 
-        # remove the player's entry in the player dictionary
-        del(players[id])
+        # rimuove il giocatore dal dizionario dei giocatori
+        del(giocatori[id])
 
-    # go through any new commands sent from players
-    for id, command, params in mud.get_commands():
+    # gestisci ogni nuovo comando mandato dai giocatori
+    for id, comando, parametri in mud.get_commands():
 
-        # if for any reason the player isn't in the player map, skip them and
-        # move on to the next one
-        if id not in players:
+        # se per qualunque ragione il giocatore non è nell'elenco, saltalo e
+        # passa al successivo
+        if id not in giocatori:
             continue
 
-        # if the player hasn't given their name yet, use this first command as
-        # their name and move them to the starting room.
-        if players[id]["name"] is None:
+        # se un giocatore non ha ancora inserito un nome, usa questo primo comando
+        # come nome e spostalo nella stanza iniziale.
+        if giocatori[id]["nome"] is None:
 
-            players[id]["name"] = command
-            players[id]["room"] = "Tavern"
+            giocatori[id]["nome"] = comando
+            giocatori[id]["stanza"] = "Taverna"
 
-            # go through all the players in the game
-            for pid, pl in players.items():
-                # send each player a message to tell them about the new player
-                mud.send_message(pid, "{} entered the game".format(
-                                                        players[id]["name"]))
+            # gestisci ogni giocatore nel gioco
+            for gid, gio in giocatori.items():
+                # manda a tutti i giocatori un messaggio per informarli dell'ingresso
+                # del nuovo giocatore
+                mud.send_message(gid, "{} è entrato nel gioco".format(
+                                                        giocatori[id]["nome"]))
 
-            # send the new player a welcome message
-            mud.send_message(id, "Welcome to the game, {}. ".format(
-                                                           players[id]["name"])
-                             + "Type 'help' for a list of commands. Have fun!")
+            # manda al nuovo giocatore un messaggio di benvenuto
+            mud.send_message(id, "Benvenuto nel gioco, {}. ".format(
+                                                           giocatori[id]["nome"])
+                             + "Scrivi 'aiuto' per una lista dei comandi. Buon divertimento!")
 
-            # send the new player the description of their current room
-            mud.send_message(id, rooms[players[id]["room"]]["description"])
+            # manda al nuovo giocatore la descrizione della stanza in cui si trova
+            mud.send_message(id, stanze[giocatori[id]["stanza"]]["descrizione"])
 
-        # each of the possible commands is handled below. Try adding new
-        # commands to the game!
+        # ogni possibile comando è gestito sotto. Prova ad aggiungere
+        # nuovi comandi al gioco!
 
-        # 'help' command
-        elif command == "help":
+        # comando 'aiuto'
+        elif comando == "aiuto":
 
-            # send the player back the list of possible commands
-            mud.send_message(id, "Commands:")
-            mud.send_message(id, "  say <message>  - Says something out loud, "
-                                 + "e.g. 'say Hello'")
-            mud.send_message(id, "  look           - Examines the "
-                                 + "surroundings, e.g. 'look'")
-            mud.send_message(id, "  go <exit>      - Moves through the exit "
-                                 + "specified, e.g. 'go outside'")
+            # restituisce al giocatore la lista dei comandi possibili
+            mud.send_message(id, "Comandi:")
+            mud.send_message(id, "  di <messaggio>  - di' qualcosa ad alta voce, "
+                                 + "ad es. 'di Ciao'")
+            mud.send_message(id, "  osserva       - Esamina il "
+                                 + "circondario, ad es. 'osserva'")
+            mud.send_message(id, "  vai <uscita>  - Muoviti verso l'uscita "
+                                 + "specificata, ad es. 'vai esterno'")
 
-        # 'say' command
-        elif command == "say":
+        # comando 'di'
+        elif comando == "di":
 
-            # go through every player in the game
-            for pid, pl in players.items():
-                # if they're in the same room as the player
-                if players[pid]["room"] == players[id]["room"]:
-                    # send them a message telling them what the player said
-                    mud.send_message(pid, "{} says: {}".format(
-                                                players[id]["name"], params))
+            # gestisci ogni giocatore nel gioco
+            for gid, gio in giocatori.items():
+                # se sono nella stessa stanze del giocatore
+                if giocatori[gid]["stanza"] == giocatori[id]["stanza"]:
+                    # manda loro un messaggio dicendo ciò che ha detto il giocatore
+                    mud.send_message(gid, "{} says: {}".format(
+                                                giocatori[id]["nome"], parametri))
 
-        # 'look' command
-        elif command == "look":
+        # comando 'osserva'
+        elif comando == "osserva":
 
-            # store the player's current room
-            rm = rooms[players[id]["room"]]
+            # memorizza la stanza corrente del giocatore
+            rm = stanze[giocatori[id]["stanza"]]
 
-            # send the player back the description of their current room
-            mud.send_message(id, rm["description"])
+            # restituisce al giocatore la descriione della stanza corrente
+            mud.send_message(id, rm["descrizione"])
 
-            playershere = []
-            # go through every player in the game
-            for pid, pl in players.items():
-                # if they're in the same room as the player
-                if players[pid]["room"] == players[id]["room"]:
-                    # ... and they have a name to be shown
-                    if players[pid]["name"] is not None:
-                        # add their name to the list
-                        playershere.append(players[pid]["name"])
+            giocatoriqui = []
+            # gestisci ogni giocatore nel gioco
+            for gid, gio in giocatori.items():
+                # se sono nella stessa stanze del giocatore
+                if giocatori[gid]["stanza"] == giocatori[id]["stanza"]:
+                    # ...e hanno un nome da mostrare
+                    if giocatori[gid]["nome"] is not None:
+                        # aggiunge il loro nome alla lista
+                        giocatoriqui.append(giocatori[gid]["nome"])
 
-            # send player a message containing the list of players in the room
-            mud.send_message(id, "Players here: {}".format(
-                                                    ", ".join(playershere)))
+            # manda al giocatore un messaggio con la lista dei giocatori nella stanza
+            mud.send_message(id, "Giocatori qui: {}".format(
+                                                    ", ".join(giocatoriqui)))
 
-            # send player a message containing the list of exits from this room
-            mud.send_message(id, "Exits are: {}".format(
-                                                    ", ".join(rm["exits"])))
+            # manda al giocatore un messaggio con la lista delle uscite della stanza
+            mud.send_message(id, "Le uscite sono: {}".format(
+                                                    ", ".join(rm["uscite"])))
 
-        # 'go' command
-        elif command == "go":
+        # comando 'vai'
+        elif comando == "vai":
 
-            # store the exit name
-            ex = params.lower()
+            # memorizza il nome dell'uscita
+            usc = parametri.lower()
 
-            # store the player's current room
-            rm = rooms[players[id]["room"]]
+            # memorizza la stanza corrente del giocatore
+            rm = stanze[giocatori[id]["stanza"]]
 
-            # if the specified exit is found in the room's exits list
-            if ex in rm["exits"]:
+            # se l'uscita specificata fa parte della lista delle uscite
+            if usc in rm["uscite"]:
 
-                # go through all the players in the game
-                for pid, pl in players.items():
-                    # if player is in the same room and isn't the player
-                    # sending the command
-                    if players[pid]["room"] == players[id]["room"] \
-                            and pid != id:
-                        # send them a message telling them that the player
-                        # left the room
-                        mud.send_message(pid, "{} left via exit '{}'".format(
-                                                      players[id]["name"], ex))
+                # gestisci ogni giocatore nel gioco
+                for gid, gio in giocatori.items():
+                    # se il giocatore è nella stessa stanza e non è
+                    # il giocatore che invia il comando
+                    if giocatori[gid]["stanza"] == giocatori[id]["stanza"] \
+                            and gid != id:
+                        # mandagli un messaggio che dice che il giocatore
+                        # ha lasciato la stanza
+                        mud.send_message(gid, "{} è andato via dall'uscita '{}'".format(
+                                                      giocatori[id]["nome"], usc))
 
-                # update the player's current room to the one the exit leads to
-                players[id]["room"] = rm["exits"][ex]
-                rm = rooms[players[id]["room"]]
+                # aggiorna la stanza corrente del giocatore a quella a cui porta l'uscita
+                giocatori[id]["stanza"] = rm["uscite"][usc]
+                rm = stanze[giocatori[id]["stanza"]]
 
-                # go through all the players in the game
-                for pid, pl in players.items():
-                    # if player is in the same (new) room and isn't the player
-                    # sending the command
-                    if players[pid]["room"] == players[id]["room"] \
-                            and pid != id:
-                        # send them a message telling them that the player
-                        # entered the room
-                        mud.send_message(pid,
-                                         "{} arrived via exit '{}'".format(
-                                                      players[id]["name"], ex))
+                # gestisci ogni giocatore nel gioco
+                for gid, gio in giocatori.items():
+                    # se il giocatore è nella stessa (nuova) stanza e non è il
+                    # giocatore che invia il comando
+                    if giocatori[gid]["stanza"] == giocatori[id]["stanza"] \
+                            and gid != id:
+                        # mandagli un messaggio che dice che il giocatore
+                        # è entrato nella stanza
+                        mud.send_message(gid,
+                                         "{} è arrivato dall'uscita '{}'".format(
+                                                      giocatori[id]["nome"], usc))
 
-                # send the player a message telling them where they are now
-                mud.send_message(id, "You arrive at '{}'".format(
-                                                          players[id]["room"]))
+                # manda al giocatore un messaggio che dice dove di trova ora
+                mud.send_message(id, "Sei arrivato in '{}'".format(
+                                                          giocatori[id]["stanza"]))
 
-            # the specified exit wasn't found in the current room
+            # l'uscita specificata non è stata trovata nella stanza
             else:
-                # send back an 'unknown exit' message
-                mud.send_message(id, "Unknown exit '{}'".format(ex))
+                # restiruisce un messaggio 'uscita sconosciuta'
+                mud.send_message(id, "Uscita sconosciuta: '{}'".format(usc))
 
-        # some other, unrecognised command
+        # qualche altro comando non riconosciuto
         else:
-            # send back an 'unknown command' message
-            mud.send_message(id, "Unknown command '{}'".format(command))
+            # restituisce un messaggio 'comando sconosciuto'
+            mud.send_message(id, "Comando sconosciuto: '{}'".format(comando))
